d7d1f845fa31793c1ba230d3c6d3b1d3477b3ba2
setrar <setrar@valiha.com>
2023-08-08 21:07:57 -0400

    .

---
+++ courses/kutz/_12/convergence.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.linalg
+from numpy.fft import fft, fftshift, ifft
+
+n=200
+x = np.linspace(0, 2*np.pi, n)
+
+s1 = np.sin(1*x); s1=s1
+s2 = np.sin(2*x)*2.5; s2=s2
+s3 = np.sin(3*x)*5; s3=s3
+s4 = np.sin(4*x)*7.5; s4=s4
+
