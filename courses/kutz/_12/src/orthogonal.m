e03df89b36958524e8c14dffcac52642585f782e
setrar <setrar@valiha.com>
2023-08-08 21:33:15 -0400

    .

--- courses/kutz/_12/src/orthogonal.m
+++ courses/kutz/_12/src/orthogonal.m
@@ -7,3 +7,10 @@ s2=sin(2.2*x/2); s2=s2/norm(s2);
 s3=sin(3*x/2); s3=s3/norm(s3);
 s4=sin(4*x/2); s4=s4/norm(s4);
 
+f=exp(-2*(x-pi+1).^2);
+
+a1=f*s1'; f1=a1*s1;
+a2=f*s2'; f2=a1*s1+a2*s2;
+a3=f*s3'; f3=a1*s1+a2*s2+a3*s3;
+a4=f*s4'; f4=a1*s1+a2*s2+a3*s3+a4*s4;
+
--- courses/kutz/_12/src/orthogonal.py
+++ courses/kutz/_12/src/orthogonal.py
@@ -11,3 +11,10 @@ s2 = np.sin(2*x)*2.5; s2=s2
 s3 = np.sin(3*x)*5; s3=s3
 s4 = np.sin(4*x)*7.5; s4=s4
 
+f = np.exp(-2*np.square(x-np.pi+1))
+
+a1=np.dot(f,s1); f1=a1*s1
+a2=np.dot(f,s2); f2=a1*s1+a2*s2
+a3=np.dot(f,s3); f3=a1*s1+a2*s2+a3*s3
+a4=np.dot(f,s4); f4=a1*s1+a2*s2+a3*s3+a4*s4
+
