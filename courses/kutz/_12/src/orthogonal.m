4ee0ba3f08ac8bbdba286bd0ae37623ecd1269c6
setrar <setrar@valiha.com>
2023-09-27 17:19:00 +0200

    .

--- courses/kutz/_12/src/orthogonal.m
+++ courses/kutz/_12/src/orthogonal.m
@@ -1,16 +1,14 @@
-
 n=200;
 x=linspace(0,2*pi,n);
 
-s1=sin(1*x/2); s1=s1/norm(s1);
-s2=sin(2.2*x/2); s2=s2/norm(s2);
-s3=sin(3*x/2); s3=s3/norm(s3);
-s4=sin(4*x/2); s4=s4/norm(s4);
-
 f=exp(-2*(x-pi+1).^2);
 
-a1=f*s1'; f1=a1*s1;
-a2=f*s2'; f2=a1*s1+a2*s2;
-a3=f*s3'; f3=a1*s1+a2*s2+a3*s3;
-a4=f*s4'; f4=a1*s1+a2*s2+a3*s3+a4*s4;
-
+approx=0*x;
+for j=1:30
+    s=sin(j*x/2); s=s/norm(s);
+    a=f*s';
+    approx=approx+a*s;
+    plot(x,f,'b',x,approx,'g','Linewidth',2)
+    title(num2str(j))
+    pause(1)
+ end
